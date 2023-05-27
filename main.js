4382278577b65a21932cde72102433397b3f4c6d
John Doe <mmd.ayin.2020@gmail.com>
2023-05-27 17:20:22 +0330

    Add getCurrnetTime

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const clock = document.querySelector('.clock');
+
+const getCurrentTime = () => {
+    let now = new Date();
+    // Hour : 0 -23  Minute , Second : 0 - 59 
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    return {
+        hour,
+        minute,
+        second
+    }
+}
+
